44d6a0330a3a92f70e88c33700eaba8dd85d481d
Michaeldean147 <michaeldean147@yahoo.com>
2014-11-26 11:46:59 -0800

    finding commit

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,7 @@
 class PeopleController < ApplicationController
 
   def index
-    @people = Person.all
+    @people = Person.where(awesome: true).where(eye_color: "green" ).order(:first_name)
   end
 
 end
